b51290a3406e3066309a78d5279fbeb7afe6c3cb
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-04-11 23:52:43 +0530

    initial commit

---
+++ 1528.cpp
@@ -0,0 +1,16 @@
+//shuffle string
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int n = s.length();
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>n;
+    }
+    for(int i=0;i<n;i++){
+         
+    }
+    return 0;
+}
