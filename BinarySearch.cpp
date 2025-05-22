208cb0db91fbb1cb83d10f8736fc5be9bbc6f026
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-05-22 15:36:12 +0530

    initial commit

---
+++ BinarySearch.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;  //array size
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target = 5;
+    int low=0;
+    int high = (n-1);
+    bool flag = false; //notfound
+    
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(arr[mid]==target){
+             flag = true;
+             cout<<"found";
+             break;
+        } 
+        else if(arr[mid]>target) high = mid-1;
+        else if(arr[mid]<target) low = mid+1;
+        else{
+            flag = false;
+            cout<<"not found";
+            break;
+        } 
+    }
+    return -1;
+}
