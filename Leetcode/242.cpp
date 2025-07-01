1fcd317676e985d36b7b6f2b516453f208422f33
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-01 23:53:43 +0530

    initial commit

---
+++ Leetcode/242.cpp
@@ -0,0 +1,22 @@
+//valid anagram
+#include<iostream>
+#include<algorithm>
+#include<unordered_map>
+using namespace std;
+int main(){
+    string s = "anagram";
+    string t = "nagaram";
+    unordered_map<char,int>map1;
+    pair<string,int>p1;
+    pair<string,int>p2;
+    unordered_map<char,int>map2;
+
+//     for(int i=0;i<s.length();i++){
+//         map1.insert(s[i]);
+//     }
+//     for(int i=0;i<s.length();i++){
+//         map2.insert(t[i]);
+//     }
+
+//     for(int i=0;i<)
+// }
