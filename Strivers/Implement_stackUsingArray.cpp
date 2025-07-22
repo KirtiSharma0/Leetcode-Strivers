6a964529cacf5c0114405f40c17cfa2d4655d0b8
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-22 12:07:30 +0530

    initial commit

---
+++ Strivers/Implement_stackUsingArray.cpp
@@ -0,0 +1,39 @@
+//implement stack using array
+#include<iostream>
+using namespace std;
+class Stack {
+public:
+    int arr[5];
+    int idx = -1;
+    void push(int value){
+        idx++;
+        arr[idx] = value;
+    }
+    void pop(){
+        idx--;
+    }
+    int top(){
+        return arr[idx];
+    }
+    int size(){
+        return idx+1;
+    }
+};
+int main(){
+    Stack st;
+    st.push(41);
+    st.push(42);
+    st.push(43);
+    st.push(44);
+    cout<<st.size()<<endl;
+
+    st.pop();
+    cout<<st.size()<<endl;
+
+    cout<<st.top()<<endl;
+
+    st.push(65);
+    cout<<st.top()<<endl;
+    cout<<st.size();
+    
+}
