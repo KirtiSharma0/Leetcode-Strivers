b076f3055bcfc854f418a9f94fdb25435441190a
KirtiSharma0 <kirtisharma5758@gmail.com>
2025-07-24 12:12:49 +0530

    initial commit

---
+++ Leetcode/2942.cpp
@@ -0,0 +1,11 @@
+//find words containing character [abc , bcd,aaa,cbc] char='a' op = [0,2]
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s = ["abc","bcd","aaaa","cbc"];
+    int n = s.length();
+    for(int i=0;i<s.length();i++){
+
+    }
+}
